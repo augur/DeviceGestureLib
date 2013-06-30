ab41ab8f53b03b338bf6b8de6c242964bf92244e
augur <kdenis87@gmail.com>
2013-07-01 00:02:48 +0400

    Initial commit of "experimental" branch.

---
+++ src/info/augury/devicegesturelib/GestureSnapshot.java
@@ -0,0 +1,9 @@
+package info.augury.devicegesturelib;
+
+/**
+ * Immutable data structure, which contains record of single execution of Device Gesture,
+ * and further assembles in {@link DeviceGestureModel} 
+ */
+public class GestureSnapshot {
+
+}
